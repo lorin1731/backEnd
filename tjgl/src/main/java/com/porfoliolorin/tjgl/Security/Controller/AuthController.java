9e8dfcec379a1321f13af5723cd2215a0debe0d2
lorin1731 <lorin.thilus@gmail.com>
2023-01-09 16:40:20 -0300

    Corrigiendo CORS en el backend

--- tjgl/src/main/java/com/porfoliolorin/tjgl/Security/Controller/AuthController.java
+++ tjgl/src/main/java/com/porfoliolorin/tjgl/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = " ")
+@CrossOrigin(origins = "**")
 public class AuthController {
 
     @Autowired
